234f97ceb797b0c309a960f5effd2779bd4d2d27
frostbite8283 <98437287+frostbite8283@users.noreply.github.com>
2024-10-30 21:13:41 -0400

    Update data parser to have enum values

--- Penn_Action/matToCSV.py
+++ Penn_Action/matToCSV.py
@@ -4,20 +4,36 @@ import scipy.io
 
 joints =  ["head","left_shoulder","right_shoulder","left_elbow","right_elbow","left_wrist","right_wrist","left_hip","right_hip","left_knee","right_knee","left_ankle","right_ankle"]
 fieldnames = joints + ["action"]
-csvPath = "InputTable" + '.csv'
+actions = {"baseball_pitch" : 0,  "clean_and_jerk": 1,  "pullup" : 2, "strum_guitar" : 3,
+"baseball_swing" : 4, "golf_swing" : 5, "pushup" : 6,  "tennis_forehand" : 7,
+"bench_press" : 8,  "jumping_jacks": 9,   "situp" : 10,   "tennis_serve" : 11,
+"bowl" : 12,  "jump_rope" : 13, "squat" : 14}
+csvTrainPath = "TrainTable" + '.csv'
+csvTestPath = "TestTable" + '.csv'
 
 rows = []
+testRows = []
 for filename in os.listdir('labels'):
     mat = scipy.io.loadmat('labels/' + filename)
 
     
     temp = {}
+    test = mat["train"][0][0]
     for i in range(len(joints)):
         temp[joints[i]] = [str(mat["x"][0][i]),str(mat["y"][0][i]),str(mat["visibility"][0][i])]
-    temp["action"] = str(mat["action"][0])
-    rows += [temp]
+    temp["action"] = actions[str(mat["action"][0])]
+    if mat["train"][0][0] == -1:
+         rows += [temp]
+    else:
+        testRows += [temp]
 print(rows)
-with open(csvPath, 'w') as f1:
+with open(csvTrainPath, 'w') as f1:
+       writer = csv.DictWriter(f1,fieldnames=fieldnames)
+       writer.writeheader()
+       for entry in rows:
+           writer.writerow(entry)
+
+with open(csvTestPath, 'w') as f1:
        writer = csv.DictWriter(f1,fieldnames=fieldnames)
        writer.writeheader()
        for entry in rows:
